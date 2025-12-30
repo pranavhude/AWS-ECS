5dec32a8cb7418dc6f838882c4079e814094c589
Pranav Hude <pranavhude20@gmail.com>
2025-12-30 14:31:45 +0530

    update5

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -9,7 +9,7 @@ resource "aws_lb_target_group" "this" {
   name     = "ecs-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id  = var.vpc_id
+  vpc_id   = var.vpc_id
 }
 
 resource "aws_lb_listener" "this" {
