3c09a0e5ae40a220a22bace3dc2105c370b246c2
Pranav Hude <pranavhude20@gmail.com>
2026-01-02 16:07:29 +0530

    updatecode

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -1,7 +1,9 @@
-resource "aws_instance" "this" {
-  ami                    = "ami-0a8e758f5e873d1c1"
-  instance_type          = "t3.micro"
-  subnet_id              = var.public_subnet
-  vpc_security_group_ids = [var.bastion_sg]
-  key_name               = var.key_name
+data "aws_ssm_parameter" "amazon_linux" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+resource "aws_instance" "bastion" {
+  ami = data.aws_ssm_parameter.amazon_linux.value
+  instance_type = "t3.micro"
+  ...
 }
